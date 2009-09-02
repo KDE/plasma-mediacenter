65109ca76400dd3d709a73d36008b4b436097626
Alessandro Diaferia <alediaferia@gmail.com>
2009-09-02 09:56:36 +0000

    start a development ground for the plugin system of the mediabrowser
    svn path=/trunk/playground/base/plasma/MediaCenterComponents/; revision=1018886

---
+++ applets/mediabrowser/mediabrowserlibs/mediabrowserabstractmodel.cpp
@@ -0,0 +1,5 @@
+#include "mediabrowserabstractmodel.h"
+
+MediaBrowserAbstractModel::MediaBrowserAbstractModel()
+{
+}
---
+++ applets/mediabrowser/mediabrowserlibs/mediabrowserabstractmodel.h
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *   Copyright 2009 by Alessandro Diaferia <alediaferia@gmail.com>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+#ifndef MEDIABROWSERABSTRACTMODEL_H
+#define MEDIABROWSERABSTRACTMODEL_H
+
+#include <QObject>
+
+/**
+ * @class MediaBrowserAbstractModel @author Alessandro Diaferia
+ * @brief A useful class to build custom plugins for the mediabrowser.
+ *
+ * This class allows you to write custom models to use with the mediabrowser applet.
+ * Custom models are useful to implement custom navigations such as navigation
+ * through services available through the web, see YouTube.
+ *
+ * This class does not direclty inherit from QAbstractItemModel due to future
+ * extension purposes. It just represents an interface between the applet and the
+ * provided model.
+ */
+class MEDIABROWSER_EXPORT MediaBrowserAbstractModel : public QObject
+{
+    Q_OBJECT
+public:
+    enum BrowsingType {
+        LocalBrowsing,
+        RemoteBrowsing
+    }
+
+    /**
+     * The ctor. Nothing special here.
+     */
+    MediaBrowserAbstractModel(QObject *parent);
+    ~MediaBrowserAbstractModel();
+
+    /**
+     * This method must be reimplemented in order to provide the model to be
+     * used by the view.
+     */
+    virtual QAbstraItemModel *model() = 0;
+
+    /**
+     * @return the browsing type of the model. Typically a remote browsing type
+     * is specific for web services like YouTube.
+     * When using a LocalBrowsing type the expected model is generally a KDirModel
+     * or an inheritance of it.
+     * Specifying a BrowsingType allows also using special widgets for navigations purposes
+     * that allow actions like "go backward", "go forward", "go uplevel" that generally change
+     * their meaning if the browsing type is local or remote.
+     */
+    virtual BrowsingType browsingType() = 0;
+};
+
+#endif // MEDIABROWSERABSTRACTMODEL_H
