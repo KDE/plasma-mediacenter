dba71bc2068cf282c848f0636d8fa0ba568207c8
Albert Astals Cid <tsdgeos@terra.es>
2011-07-12 16:35:21 +0100

    Fix Messages.sh
    You do not have .ui files
    You can not use -j
    CCMAIL: notmart@gmail.com

--- containments/mediacontainment/Messages.sh
+++ containments/mediacontainment/Messages.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui` >> rc.cpp
-$XGETTEXT `find . -name \*qml` -j -L Java -o $podir/org.kde.mediacontainment.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/org.kde.mediacontainment.pot
 rm -f rc.cpp
