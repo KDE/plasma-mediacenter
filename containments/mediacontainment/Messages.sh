99f9f2d8e2c09b9274bee40039ada84884c8a2d8
Marco Martin <notmart@gmail.com>
2011-11-02 14:53:48 +0100

    new catalog name

--- containments/mediacontainment/Messages.sh
+++ containments/mediacontainment/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/org.kde.mediacontainment.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_org.kde.mediacontainment.pot
 rm -f rc.cpp
