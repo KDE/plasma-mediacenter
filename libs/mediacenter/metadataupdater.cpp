e3c2cdceec09942e33ffb5af308711622f2be55c
Shantanu Tushar <shantanu@kde.org>
2012-10-17 00:36:30 +0530

    No moar than 2000 items please

--- libs/mediacenter/metadataupdater.cpp
+++ libs/mediacenter/metadataupdater.cpp
@@ -33,6 +33,8 @@
 
 #include <QtCore/QTimer>
 
+static const int s_queryLimit = 2000;
+
 MetadataUpdater::MetadataUpdater(const QList< int >& rolesRequested, QObject* parent)
     : QThread(parent), m_rolesRequested(rolesRequested), m_termChanged(false), m_queryServiceClient(0)
 {
@@ -73,6 +75,7 @@ void MetadataUpdater::runQuery()
     m_resultList.clear();
 
     Nepomuk::Query::Query myQuery;
+    myQuery.setLimit(s_queryLimit);
     if (m_queryServiceClient) {
         m_queryServiceClient->close();
         m_queryServiceClient->deleteLater();
