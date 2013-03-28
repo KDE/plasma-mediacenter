ca1ce57931d3b1b81a691561197cb4449a44dab3
Gregor Tätzner <gregor@freenet.de>
2013-03-28 23:29:18 +0100

    add license headers

--- browsingbackends/onlineservices/rssfeed/rssbackend.cpp
+++ browsingbackends/onlineservices/rssfeed/rssbackend.cpp
@@ -12,7 +12,25 @@
 #include <Akonadi/ItemFetchScope>
 #include <Akonadi/AgentManager>
 #include <Akonadi/AgentInstanceCreateJob>
-
+/*
+ * Copyright 2013 Gregor Taetzner gregor@freenet.de
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <krss/feedcollection.h>
 #include <KRss/Item>
 
