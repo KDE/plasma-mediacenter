23808ece0a730afbc0d9e6719cc8ae58153d88ab
Yuri Chornoivan <yurchor@ukr.net>
2013-05-11 10:45:01 +0300

    Fix template name

--- Messages.sh
+++ Messages.sh
@@ -5,5 +5,5 @@
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/APPNAME.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/plasma-mediacenter.pot
 
