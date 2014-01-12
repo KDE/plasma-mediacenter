f59b1376214ac212c845f8c45954d9c7866da9e7
Shantanu Tushar <shantanu@kde.org>
2014-01-12 11:01:22 +0530

    Rename Media to MediaImpl

--- libs/mediacenter/pmcmetadatamodel.cpp
+++ libs/mediacenter/pmcmetadatamodel.cpp
@@ -125,11 +125,11 @@ void PmcMetadataModel::showMediaType(MediaCenter::MediaType mediaType)
         case MediaCenter::Video:
             type = "video";
     }
-    QList <QSharedPointer<Media> > mediaData = MediaLibrary::instance()->getMedia(type);
+    QList <QSharedPointer<MediaImpl> > mediaData = MediaLibrary::instance()->getMedia(type);
 
     beginResetModel();
     int i = 0;
-    foreach (QSharedPointer<Media> m, mediaData) {
+    foreach (QSharedPointer<MediaImpl> m, mediaData) {
         QHash<int, QVariant> mediainfo;
         mediainfo.insert(Qt::DisplayRole, m->title());
         mediainfo.insert(Qt::DecorationRole, m->thumbnail());
