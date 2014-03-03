7b187d2df4b4af6c6ceb47ca10737812c2e8e381
Shantanu Tushar <shantanu@kde.org>
2014-03-03 16:48:30 +0530

    Add FindQxOrm.cmake to find QxOrm

---
+++ cmake/FindQxOrm.cmake
@@ -0,0 +1,27 @@
+# - Try to find QxOrm
+# Once done, this will define
+#
+#  QxOrm_FOUND - system has QxOrm
+#  QxOrm_INCLUDE_DIRS - the QxOrm include directories
+#  QxOrm_LIBRARIES - link these to use QxOrm
+
+include(LibFindMacros)
+
+# Include dir
+find_path(QxOrm_INCLUDE_DIR
+  NAMES QxOrm.h
+#  PATHS ${QxOrm_PKGCONF_INCLUDE_DIRS}
+)
+
+# Finally the library itself
+find_library(QxOrm_LIBRARY
+  NAMES QxOrm
+#  PATHS ${QxOrm_PKGCONF_LIBRARY_DIRS}
+)
+
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
+set(QxOrm_PROCESS_INCLUDES QxOrm_INCLUDE_DIR QxOrm_INCLUDE_DIRS)
+set(QxOrm_PROCESS_LIBS QxOrm_LIBRARY QxOrm_LIBRARIES)
+libfind_process(QxOrm)
+
