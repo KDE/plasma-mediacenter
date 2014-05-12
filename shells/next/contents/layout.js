5eecbe169f11222bf10b6252499a367872d1a06c
Bhushan Shah <bhush94@gmail.com>
2014-05-13 00:47:35 +0530

    Set wallpaper to color instead of image

--- shells/next/contents/layout.js
+++ shells/next/contents/layout.js
@@ -13,7 +13,8 @@ for (var i = 0; i < screenCount; ++i) {
     var desktopsArray = desktopsForActivity(id);
     print(desktopsArray.length);
     for( var j = 0; j < desktopsArray.length; j++) {
-        desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+        desktopsArray[j].wallpaperPlugin = "org.kde.color";
+        print(desktopsArray[j].wallpaperPlugin);
         //var clock = desktopsArray[j].addWidget("org.kde.plasma.analogclock");
     }
 }
