55c5bc5b3621da83764213cda9b7e143db7782d3
Bhushan Shah <bhush94@gmail.com>
2014-06-19 15:32:54 +0530

    Add panel

--- shells/next/contents/layout.js
+++ shells/next/contents/layout.js
@@ -1,12 +1,12 @@
 
-//var panel = new Panel
-//panel.screen = 0
-//panel.location = 'top'
-//panel.addWidget("org.kde.plasma.kickoff")
-//panel.addWidget("org.kde.plasma.pager")
-//panel.addWidget("org.kde.plasma.taskmanager")
-//panel.addWidget("org.kde.plasma.systemtray")
-//panel.addWidget("org.kde.plasma.digitalclock")
+var panel = new Panel
+panel.screen = 0
+panel.location = 'top'
+panel.addWidget("org.kde.plasma.kickoff")
+panel.addWidget("org.kde.plasma.pager")
+panel.addWidget("org.kde.plasma.taskmanager")
+panel.addWidget("org.kde.plasma.systemtray")
+panel.addWidget("org.kde.plasma.digitalclock")
 
 for (var i = 0; i < screenCount; ++i) {
     var id = createActivity("Plasma mediacenter");
