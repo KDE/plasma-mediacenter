78d67f75971de98a4005a8db78919245fff6c972
Bhushan Shah <bhush94@gmail.com>
2015-02-17 22:58:58 +0530

    Allow packager/developer to configure startkde location
    override STARTKDE_BINARY_DIR if your plasma-workspace and
    plasma-mediacenter is not installed in same prefix

--- plasma-mediacenter.desktop.cmake
+++ plasma-mediacenter.desktop.cmake
@@ -1,8 +1,8 @@
 [Desktop Entry]
 Encoding=UTF-8
 Type=XSession
-Exec=${CMAKE_INSTALL_FULL_BINDIR}/startkde
-TryExec=${CMAKE_INSTALL_FULL_BINDIR}/startkde
+Exec=${STARTKDE_BINARY_DIR}/startkde
+TryExec=${STARTKDE_BINARY_DIR}/startkde
 DesktopNames=KDE
 Name=Plasma Media Center
 Comment=Plasma Media Center by KDE
