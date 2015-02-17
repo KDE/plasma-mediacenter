451eceffaf4a1c06e162dc629bb5fc945e40ae73
Bhushan Shah <bhush94@gmail.com>
2015-02-17 22:50:08 +0530

    Install session file for pmc
    For that session file to work it will require recent plasma-workspace
    with commit 77112d402e3c22311dd2d70f9d2b264d1465cf03

---
+++ plasma-mediacenter.desktop.cmake
@@ -0,0 +1,9 @@
+[Desktop Entry]
+Encoding=UTF-8
+Type=XSession
+Exec=${CMAKE_INSTALL_FULL_BINDIR}/startkde
+TryExec=${CMAKE_INSTALL_FULL_BINDIR}/startkde
+DesktopNames=KDE
+Name=Plasma Media Center
+Comment=Plasma Media Center by KDE
+X-KDE-PluginInfo-Version=${PROJECT_VERSION}
