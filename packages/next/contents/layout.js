49261d44318db78261731cabdcc4a82e75905349
Bhushan Shah <bhush94@gmail.com>
2015-05-10 13:13:02 +0530

    Add comic applet too in demo

--- packages/next/contents/layout.js
+++ packages/next/contents/layout.js
@@ -4,7 +4,8 @@ for (var i = 0; i < screenCount; ++i) {
     print(desktopsArray.length);
     for( var j = 0; j < desktopsArray.length; j++) {
         desktopsArray[j].wallpaperPlugin = 'org.kde.image';
-        var clock = desktopsArray[j].addWidget("org.kde.plasma.analogclock");
-        var clock = desktopsArray[j].addWidget("org.kde.plasma.digitalclock");
+        desktopsArray[j].addWidget("org.kde.plasma.analogclock");
+        desktopsArray[j].addWidget("org.kde.plasma.digitalclock");
+        desktopsArray[j].addWidget("org.kde.plasma.comic");
     }
 }
